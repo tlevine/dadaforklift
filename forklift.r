5fedc9590c9f898bbaa1704165cda6ba61181c22
Thomas Levine <_@thomaslevine.com>
2014-09-25 21:12:05 +0000

    emails

--- forklift.r
+++ forklift.r
@@ -1,3 +1,9 @@
 library(sqldf)
 
 q <- function(sql) sqldf(sql, drv = 'RPostgreSQL', dbname = 'tlevine')
+
+# select "serverDateTime", "visitorId", "visitIp" from dim_piwikvisit;
+emails.from <- function(address) {
+  sql <- paste0("select * from dim_emailmessage where from_address = '", address, "';")
+  q(sql)
+}
